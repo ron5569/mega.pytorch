a66c8318d691d03ccfc55abbd6da01d1f271b927
indoordesk <ron.yadgar@indoor-robotics.com>
2020-04-09 21:14:29 +0300

    update

--- mega_core/data/datasets/vid.py
+++ mega_core/data/datasets/vid.py
@@ -19,24 +19,28 @@ from mega_core.utils.comm import is_main_process
 
 
 class VIDDataset(torch.utils.data.Dataset):
-    classes = ['__background__',  # always index 0
-                'airplane', 'antelope', 'bear', 'bicycle',
-                'bird', 'bus', 'car', 'cattle',
-                'dog', 'domestic_cat', 'elephant', 'fox',
-                'giant_panda', 'hamster', 'horse', 'lion',
-                'lizard', 'monkey', 'motorcycle', 'rabbit',
-                'red_panda', 'sheep', 'snake', 'squirrel',
-                'tiger', 'train', 'turtle', 'watercraft',
-                'whale', 'zebra']
-    classes_map = ['__background__',  # always index 0
-                    'n02691156', 'n02419796', 'n02131653', 'n02834778',
-                    'n01503061', 'n02924116', 'n02958343', 'n02402425',
-                    'n02084071', 'n02121808', 'n02503517', 'n02118333',
-                    'n02510455', 'n02342885', 'n02374451', 'n02129165',
-                    'n01674464', 'n02484322', 'n03790512', 'n02324045',
-                    'n02509815', 'n02411705', 'n01726692', 'n02355227',
-                    'n02129604', 'n04468005', 'n01662784', 'n04530566',
-                    'n02062744', 'n02391049']
+    # classes = ['__background__',  # always index 0
+    #             'airplane', 'antelope', 'bear', 'bicycle',
+    #             'bird', 'bus', 'car', 'cattle',
+    #             'dog', 'domestic_cat', 'elephant', 'fox',
+    #             'giant_panda', 'hamster', 'horse', 'lion',
+    #             'lizard', 'monkey', 'motorcycle', 'rabbit',
+    #             'red_panda', 'sheep', 'snake', 'squirrel',
+    #             'tiger', 'train', 'turtle', 'watercraft',
+    #             'whale', 'zebra']
+    # classes_map = ['__background__',  # always index 0
+    #                 'n02691156', 'n02419796', 'n02131653', 'n02834778',
+    #                 'n01503061', 'n02924116', 'n02958343', 'n02402425',
+    #                 'n02084071', 'n02121808', 'n02503517', 'n02118333',
+    #                 'n02510455', 'n02342885', 'n02374451', 'n02129165',
+    #                 'n01674464', 'n02484322', 'n03790512', 'n02324045',
+    #                 'n02509815', 'n02411705', 'n01726692', 'n02355227',
+    #                 'n02129604', 'n04468005', 'n01662784', 'n04530566',
+    #                 'n02062744', 'n02391049']
+
+    classes = ['__background__', "person"]
+
+    classes_map = ['0.0', 'person']
 
     def __init__(self, image_set, data_dir, img_dir, anno_path, img_index, transforms, is_train=True):
         self.det_vid = image_set.split("_")[0]
@@ -50,7 +54,8 @@ class VIDDataset(torch.utils.data.Dataset):
 
         self.is_train = is_train
 
-        self._img_dir = os.path.join(self.img_dir, "%s.JPEG")
+        #self._img_dir = os.path.join(self.img_dir, "%s.JPEG")
+        self._img_dir = os.path.join(self.img_dir, "%s.jpg")
         self._anno_path = os.path.join(self.anno_path, "%s.xml")
 
         with open(self.img_index) as f:
@@ -82,6 +87,7 @@ class VIDDataset(torch.utils.data.Dataset):
         self.categories = dict(zip(range(len(self.classes)), self.classes))
 
         self.annos = self.load_annos(os.path.join(self.cache_dir, self.image_set + "_anno.pkl"))
+        #self.annos = []
 
     def __getitem__(self, idx):
         if self.is_train:
@@ -192,6 +198,7 @@ class VIDDataset(torch.utils.data.Dataset):
         return annos
 
     def get_img_info(self, idx):
+        #print(idx)
         im_info = self.annos[idx]["im_info"]
         return {"height": im_info[0], "width": im_info[1]}
 
